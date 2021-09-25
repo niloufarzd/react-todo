1da1406683e1ad9a66eca7f34d49ab7fd946baf9
niloufar <zaferaniniloufar@gmail.com>
2021-09-25 09:42:25 +0330

    complate project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TaskList from './TaskList';
 import './App.css';
 
-function App() {
+ const App = () => {
+  const [newTaskText,setNewTaskText] = useState("");
+  const [tasks,setTasks] = useState([
+  ]);
+
+  const addTask=()=>{
+    if(!newTaskText)
+      return;
+    setTasks(currentTasks=>{
+      return [...currentTasks,newTaskText];
+    })
+    setNewTaskText("")
+  };
+
+
+  const onChangeInput=(e)=>{
+    const value =e.target.value;
+    setNewTaskText(value);
+  };
+
   return (
-    <div className="App">
-     <h1>slam</h1>
+    <div className={"container"}>
+      <input value={newTaskText} onChange={onChangeInput}/><button onClick={addTask}>add</button>
+      <TaskList tasks={tasks}/>
     </div>
   );
-}
-
-export default App;
+};
+ export default App;
---
+++ src/TaskList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TaskList = ({tasks}) => {
+  return (
+    <div className={"list"}>
+      {tasks.map(item=> <TaskItem item={item}/>)}
+    </div>
+  );
+};
+
+const TaskItem = ({item})=>{
+  return <div className={"task"}>
+    <p className={"taskTitle"}>{item}</p>
+  </div>
+}
+
+export default TaskList;
